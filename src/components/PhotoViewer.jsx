9d0c339b5d54578832786364f092ce951b934a2c
Leo Yockey <leoyockey@gmail.com>
2020-07-03 16:56:34 -0700

    temp handling for videos

--- src/components/PhotoViewer.jsx
+++ src/components/PhotoViewer.jsx
@@ -27,7 +27,10 @@ export default ({date, onClose}) => {
             {!isLoading && (
                 <div>
                     <h2>{data.title}</h2>
-                    <img src={data.url} alt={'photo of ' + data.title} />
+                    {data.media_type === 'image'
+                        ? <img src={data.url} alt={'photo of ' + data.title} />
+                        : <p>Media is a video (not an image). Feature coming soon! Go back and try another date to view an image</p>
+                    }
                     <p>{data.description}</p>
                     <p>&copy; {data.copyright}</p>
                 </div>
